45e51aec3910ec9e4a7ed7e0eb7a3c552efb03e2
dev2 <dev2@dev2.alloa>
2024-07-24 15:19:54 +0200

    :fire: correction de code

---
+++ src/app/components/shared/detail-ticket/detail-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-ticket.component.html',
+  styleUrl: './detail-ticket.component.css'
+})
+export class DetailTicketComponent {
+
+}
